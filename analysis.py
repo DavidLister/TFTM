d767cbecc284726b234793f072541420b45a7e56
David Lister <cwsfdavidl@gmail.com>
2023-01-12 23:09:48 -0800

    Forgot to add the program files

---
+++ analysis.py
@@ -0,0 +1,223 @@
+
+# analysis.py
+#
+# Analysis-related code goes here
+# David Lister
+# January 2023
+#
+
+import numpy as np
+from scipy import interpolate, odr, signal
+import logging
+
+import common
+
+logger = logging.getLogger("TFTM.analysis")
+
+
+class DataContainer:
+    """
+    Class for holding data.
+
+    Variable initialized to None.
+    """
+    def __init__(self):
+        self.calibration_spectrum = None
+        self.raw_reflectance_spectrum = None
+        self.dark_reference_spectrum = None
+        self.calc_reflectance_spectrum = None
+        self.theo_reflectance_spectrum = None
+        self.thin_film_optical_properties = None
+        self.substrate_optical_properties = None
+        self.fit = None
+        self.thickness = 1000
+        self.thickness_theoretical = 1000
+        self.amplitude_theoretical = 1
+
+
+class OpticalProperties:
+    """
+    Hold optical property data
+    """
+    def __init__(self, wavelength, n, k):
+        self.lower = np.min(wavelength)
+        self.upper = np.max(wavelength)
+        self.n = interpolate.interp1d(wavelength, n)
+        self.k = interpolate.interp1d(wavelength, k)
+
+
+class Fit:
+    """
+    Holds fit data
+    """
+    def __init__(self, x, y, B, B_err):
+        self.wavelength = x
+        self.reflectance = y
+        self.params = B
+        self.error = B_err
+
+
+def load_spectrum(fname, dark_spectrum=None, dark_subtraction_enabled=True, normalize=False):
+    logger.debug("Loading a spectrum file")
+    if fname == "":
+        logger.info("No file selected")
+        return None
+    data = np.loadtxt(fname,
+                      delimiter=common.SPECTRUM_FILE_DELIMITER,
+                      skiprows=common.SPECTRUM_FILE_SKIPLINES)
+
+    count = len(data[0][common.SPECTRUM_DATA_START_ROW:])
+    data = data.transpose()
+    data_out = np.zeros((2, sum(data[common.SPECTRUM_DATA_START_ROW] > 0)))
+    data_out[0] = data[common.SPECTRUM_WAVELENGTH_ROW][data[common.SPECTRUM_DATA_START_ROW] > 0]
+
+    # Sum all the non-zero data from columns 4 to N and add it to the output data vector
+    data_out[1] = np.mean(data[common.SPECTRUM_DATA_START_ROW:, data[common.SPECTRUM_DATA_START_ROW] > 0], axis=0)
+
+    # dark_subtraction
+    if dark_spectrum is not None and dark_subtraction_enabled:
+        data_out[1] = np.abs(data_out[1] - dark_spectrum[1]) + 0.01  # Small value added for numerical instability
+
+    # Normalize the spectrum
+    if normalize:
+        data_out[1] = data_out[1] / np.max(data_out[1])
+
+    return data_out
+
+
+def can_calculate_reflectance(data_container):
+    logger.debug("Can reflectance be calculated?")
+    if not isinstance(data_container.calibration_spectrum, np.ndarray):
+        logger.debug("No - Calibration spectrum is missing")
+        return False
+
+    if not isinstance(data_container.raw_reflectance_spectrum, np.ndarray):
+        logger.debug("No - Raw reflectance spectrum missing")
+        return False
+
+    cal_shape = data_container.calibration_spectrum.shape
+    refl_shape = data_container.raw_reflectance_spectrum.shape
+    if cal_shape == refl_shape:
+        logger.debug("Yes")
+        return True
+
+    else:
+        logger.warning(f"Can't calculate reflectance, however two numpy arrays are loaded of shapes {cal_shape}, {refl_shape}")
+
+
+def calculate_reflectance(data_container):
+    logger.debug("Calculating reflectance")
+    # Assumes array shapes have already been checked
+
+    data = np.zeros(data_container.calibration_spectrum.shape)
+    data[0] = data_container.calibration_spectrum[0]
+
+    reflected = data_container.raw_reflectance_spectrum[1]
+    reflected = reflected / np.max(reflected)
+
+    calibration = data_container.calibration_spectrum[1]
+    calibration = calibration / np.max(calibration)
+
+    data[1] = reflected / calibration
+
+    # Normalize so max is 1
+    data[1] = data[1] / np.max(data[1])
+
+    # dx = data[0][1] - data[0][0]
+    # width = 300
+    # offset = signal.savgol_filter(data[1], int(width/dx), 3, mode="constant")
+    # data[1] = data[1] - offset
+
+    return data
+
+
+def load_optical_properties(fname):
+    logger.debug(f"Loading optical properties for {fname}")
+    data = np.loadtxt(fname, skiprows=common.OPTICAL_PROPS_SKIPLINES, delimiter=common.OPTICAL_PROPS_DELIMITER)
+    data = data.transpose()
+    return OpticalProperties(data[0] * 1000, data[1], data[2])
+
+
+efield_reflectance = lambda n1, n2: (n1 - n2) / (n1 + n2)
+efield_reflectance_complex = lambda n1, k1, n2, k2: ((n1 - 1j * k1) - (n2 - 1j * k2)) / ((n1 - 1j * k1) + (n2 - 1j * k2))
+
+
+def reflectance_model(wavelengths, n_air, n_tf, k_tf, n_sub, k_sub, d, A):
+    """
+    Based on equations 3.4, 3.10 and 3.6 in "A Practical Guide to Optical Metrology for Thin Films by Quinten
+    """
+    r01 = efield_reflectance_complex(n_air, 0, n_tf(wavelengths), k_tf(wavelengths))
+    r12 = efield_reflectance_complex(n_tf(wavelengths), k_tf(wavelengths), n_sub(wavelengths), k_sub(wavelengths))
+    R01 = np.real(r01 * np.conj(r01))
+    R12 = np.real(r12 * np.conj(r12))
+    phase_shift = np.arctan(np.imag(np.conj(r01) * r12) / np.real(np.conj(r01) * r12))
+
+    refl_numerator = R01 + R12 * np.exp(-(8 * np.pi / wavelengths) * k_tf(wavelengths) * d) + \
+                     2 * np.sqrt(R01 * R12) * np.exp(-(4 * np.pi / wavelengths) * k_tf(wavelengths) * d) * np.cos((4 * np.pi / wavelengths) * n_tf(wavelengths) * d + phase_shift)
+
+    refl_denom = 1 + R01 * R12 * np.exp(-(8 * np.pi / wavelengths) * k_tf(wavelengths) * d) + \
+                     2 * np.sqrt(R01 * R12) * np.exp(-(4 * np.pi / wavelengths) * k_tf(wavelengths) * d) * np.cos(
+        (4 * np.pi / wavelengths) * n_tf(wavelengths) * d + phase_shift)
+
+    return  A * refl_numerator / refl_denom
+
+
+def simple_model(wavelengths, n_air, n_tf, k_tf, n_sub, k_sub, d, A):
+    r01 = efield_reflectance_complex(n_air, 0, n_tf(wavelengths), k_tf(wavelengths))
+    r12 = efield_reflectance_complex(n_tf(wavelengths), k_tf(wavelengths), n_sub(wavelengths), k_sub(wavelengths))
+    R01 = np.real(r01 * np.conj(r01))
+    R12 = np.real(r12 * np.conj(r12))
+    phase_shift = np.arctan(np.imag(np.conj(r01) * r12) / np.real(np.conj(r01) * r12))
+    return A * np.cos((4 * np.pi / wavelengths) * n_tf(wavelengths) * d + phase_shift)
+
+
+def calculate_thickness(data_container):
+    logger.debug("Calculating film thickness")
+    # assumes data has been checked
+
+    lower = max((data_container.thin_film_optical_properties.lower,
+                 data_container.substrate_optical_properties.lower,
+                 data_container.calibration_spectrum[0][0],
+                 350))
+
+    upper = min((data_container.thin_film_optical_properties.upper,
+                 data_container.substrate_optical_properties.upper,
+                 data_container.calibration_spectrum[0][-1],
+                 800))
+
+    logger.debug(f"Bounds are: {lower}, {upper}")
+
+    mask = (upper > data_container.calibration_spectrum[0]) * (data_container.calibration_spectrum[0] > lower)
+
+    logger.debug(f"Min: {np.min(data_container.calibration_spectrum[0][mask])} -- Max: {np.max(data_container.calibration_spectrum[0][mask])}")
+
+    def fit_model(B, x):
+        """ B[0] is thickness, B[1] is scaling factor"""
+        return reflectance_model(x, 1,
+                                 data_container.thin_film_optical_properties.n,
+                                 data_container.thin_film_optical_properties.k,
+                                 data_container.substrate_optical_properties.n,
+                                 data_container.substrate_optical_properties.k,
+                                 B[0], B[1])
+
+    model = odr.Model(fit_model)
+    fit_data = odr.RealData(data_container.calc_reflectance_spectrum[0][mask],
+                            data_container.calc_reflectance_spectrum[1][mask]) # todo - No error at the moment
+
+    fit = odr.ODR(fit_data, model, beta0=[data_container.thickness, 1])
+    output = fit.run()
+    output.pprint()
+
+    fit_y = fit_model(output.beta, data_container.calc_reflectance_spectrum[0][mask])
+
+    return Fit(data_container.calc_reflectance_spectrum[0][mask], fit_y, output.beta, output.sd_beta)
+
+
+def can_calculate_thickness(data):
+    if not can_calculate_reflectance(data):
+        return False
+
+    if data.thin_film_optical_properties is not None and data.substrate_optical_properties is not None:
+        return True
+
+    return False
---
+++ tftm.py
@@ -0,0 +1,264 @@
+# tftm.py
+#
+# Thin Film Thickness Measurement (TFTM) main file
+# David Lister
+# Started January 2023
+#
+
+from PySide6 import QtGui, QtCore
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QGridLayout, QFileDialog, QSlider, QLabel
+import pyqtgraph as pg
+import sys
+# Setup Logging
+import logging
+import common
+import analysis
+
+
+logger = logging.getLogger("TFTM")
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+logger.debug("Logger Started")
+
+
+class InternalState:
+    """
+    Class to hold internal program state, separate from the gui.
+
+    All variable initiated as None type.
+    """
+    def __init__(self):
+        self.calibration_file_path = None
+        self.reflectance_file_path = None
+        self.dark_reference_file_path = None
+        self.thin_film_properties_path = None
+        self.substrate_properties_path = None
+        self.last_thickness_fit = None
+
+
+class MainWindow(QMainWindow):
+    """
+    Main GUI Class.
+    """
+    def __init__(self):
+        super().__init__()
+        self.state = InternalState()
+        self.data = analysis.DataContainer()
+        self.logger = logging.getLogger("TFTM.MainWindow")
+        self.logger.debug("Main window started")
+
+        self.setWindowTitle("TFTM")
+        self.colour = self.palette().color(QtGui.QPalette.Window)
+        self.main_pen = pg.mkPen(color=(20, 20, 20))
+        self.fit_pen = pg.mkPen(color=(153, 0, 0))
+
+        self.layout = QGridLayout()
+
+        self.button_calibration_selection = QPushButton("Select Calibration")
+        self.button_calibration_selection.clicked.connect(self.get_calibration_file)
+        self.button_calibration_selection.setEnabled(not common.SUBTRACT_DARK_FROM_CALIBRATION)
+
+        self.button_reflectance_selection = QPushButton("Select Reflected Spectrum")
+        self.button_reflectance_selection.clicked.connect(self.get_reflectance_file)
+        self.button_reflectance_selection.setEnabled(not common.SUBTRACT_DARK_FROM_REFLECTED)
+
+        self.reflectance_display = QLabel()
+        self.reflectance_display.setText(f"Reflectance file:{self.state.reflectance_file_path}")
+
+        self.button_dark_reference_selection = QPushButton("Select Dark Reference Spectrum")
+        self.button_dark_reference_selection.clicked.connect(self.get_dark_reference_file)
+
+        self.button_thin_film_material = QPushButton("Select Thin Film Material")
+        self.button_thin_film_material.clicked.connect(self.get_thin_film_material)
+
+        self.button_substrate_material = QPushButton("Select Substrate Material")
+        self.button_substrate_material.clicked.connect(self.get_substrate_material)
+
+        self.calibration_plot = pg.PlotWidget()
+        self.calibration_plot.setBackground(self.colour)
+        self.calibration_plot.setTitle("Calibration Spectrum")
+        self.calibration_plot.setLabel("bottom", "Wavelength (nm)")
+
+        self.raw_reflectance_plot = pg.PlotWidget()
+        self.raw_reflectance_plot.setBackground(self.colour)
+        self.raw_reflectance_plot.setTitle("Raw Reflected Spectrum")
+        self.raw_reflectance_plot.setLabel("bottom", "Wavelength (nm)")
+
+        self.calc_reflectance_plot = pg.PlotWidget()
+        self.calc_reflectance_plot.setBackground(self.colour)
+        self.calc_reflectance_plot.setTitle("Reflectance Spectrum")
+        self.calc_reflectance_plot.setLabel("bottom", "Wavelength (nm)")
+
+        self.thickness_slider_fit = QSlider(QtCore.Qt.Horizontal)
+        self.thickness_slider_fit.setMinimum(1)
+        self.thickness_slider_fit.setMaximum(3000)
+        self.thickness_slider_fit.setValue(1000)
+        self.thickness_slider_fit.setTracking(False)
+        self.thickness_slider_fit.valueChanged.connect(self.get_fit_slider_pos)
+
+        self.thickness_slider_theory = QSlider(QtCore.Qt.Horizontal)
+        self.thickness_slider_theory.setMinimum(1)
+        self.thickness_slider_theory.setMaximum(3000)
+        self.thickness_slider_theory.setValue(1000)
+        self.thickness_slider_theory.valueChanged.connect(self.get_theory_slider_pos)
+
+
+        self.amplitude_slider_theory = QSlider(QtCore.Qt.Horizontal)
+        self.amplitude_slider_theory.setMinimum(0.1)
+        self.amplitude_slider_theory.setMaximum(100)
+        self.amplitude_slider_theory.setValue(1)
+        self.amplitude_slider_theory.valueChanged.connect(self.get_theory_amplitude_slider_pos)
+
+        self.thickness_display = QLabel()
+        self.thickness_display.setText(f"Fit Thickness: {self.data.thickness} nm")
+
+        self.thickness_theory_display = QLabel()
+        self.thickness_theory_display.setText(f"Theoretical Model Thickness: {self.data.thickness_theoretical} nm")
+
+        self.amplitude_theory_display = QLabel()
+        self.amplitude_theory_display.setText(f"Theoretical Model Amplitude: {self.data.amplitude_theoretical}")
+
+        self.layout.addWidget(self.button_calibration_selection, 0, 0)
+        self.layout.addWidget(self.button_reflectance_selection, 0, 1)
+        self.layout.addWidget(self.button_dark_reference_selection, 0, 2)
+        self.layout.addWidget(self.button_thin_film_material, 0, 4)
+        self.layout.addWidget(self.button_substrate_material, 0, 5)
+        self.layout.addWidget(self.reflectance_display, 1, 1, 1, 4)
+        self.layout.addWidget(self.calibration_plot, 2, 0, 3, 3)
+        self.layout.addWidget(self.raw_reflectance_plot, 2, 4, 3, 3)
+        self.layout.addWidget(self.calc_reflectance_plot, 7, 0, 5, 7)
+        self.layout.addWidget(self.thickness_slider_fit, 15, 0, 1, 7)
+        self.layout.addWidget(self.thickness_display, 16, 3)
+        self.layout.addWidget(self.thickness_slider_theory, 17, 0, 1, 7)
+        self.layout.addWidget(self.thickness_theory_display, 18, 3)
+        self.layout.addWidget(self.amplitude_slider_theory, 19, 0, 1, 7)
+        self.layout.addWidget(self.amplitude_theory_display, 20, 3)
+
+        self.widget = QWidget()
+        self.widget.setLayout(self.layout)
+        self.setCentralWidget(self.widget)
+
+    def get_calibration_file(self):
+        self.logger.debug("Getting calibration file")
+        self.state.calibration_file_path = QFileDialog.getOpenFileName()[0]
+        self.logger.info(f"Calibration file set to '{self.state.calibration_file_path}'")
+        self.data.calibration_spectrum = analysis.load_spectrum(self.state.calibration_file_path,
+                                                                self.data.dark_reference_spectrum,
+                                                                common.SUBTRACT_DARK_FROM_CALIBRATION)
+        if self.data.calibration_spectrum is not None:
+            self.logger.debug("Calibration loaded")
+            self.calibration_plot.clear()
+            self.calibration_plot.plot(self.data.calibration_spectrum[0], self.data.calibration_spectrum[1], pen=self.main_pen)
+            self.draw_calculated_reflectance()
+
+        else:
+            self.logger.info("Error Loading calibration file")
+
+    def get_reflectance_file(self):
+        self.logger.debug("Getting reflectance spectrum file")
+        self.state.reflectance_file_path = QFileDialog.getOpenFileName()[0]
+        self.reflectance_display.setText(f"Reflectance file:{self.state.reflectance_file_path}")
+        self.logger.info(f"Reflectance file set to '{self.state.reflectance_file_path}'")
+        self.data.raw_reflectance_spectrum = analysis.load_spectrum(self.state.reflectance_file_path,
+                                                                    self.data.dark_reference_spectrum,
+                                                                    common.SUBTRACT_DARK_FROM_REFLECTED)
+        if self.data.raw_reflectance_spectrum is not None:
+            self.logger.debug("Raw reflectance loaded")
+            self.raw_reflectance_plot.clear()
+            self.raw_reflectance_plot.plot(self.data.raw_reflectance_spectrum[0], self.data.raw_reflectance_spectrum[1], pen=self.main_pen)
+            self.draw_calculated_reflectance()
+
+        else:
+            logger.info("Error loading reflectance file")
+
+    def get_dark_reference_file(self):
+        self.logger.debug("Getting dark reference spectrum file")
+        self.state.dark_reference_file_path = QFileDialog.getOpenFileName()[0]
+        self.logger.info(f"Dark reference file set to '{self.state.dark_reference_file_path}'")
+        self.data.dark_reference_spectrum = analysis.load_spectrum(self.state.dark_reference_file_path)
+        self.logger.debug("Dark reference loaded")
+        self.button_calibration_selection.setEnabled(True)
+        self.button_reflectance_selection.setEnabled(True)
+        self.draw_calculated_reflectance()
+
+    def draw_calculated_reflectance(self):
+        self.logger.debug("Drawing calculated reflectance")
+        if analysis.can_calculate_reflectance(self.data):
+            self.data.calc_reflectance_spectrum = analysis.calculate_reflectance(self.data)
+            if self.state.last_thickness_fit != self.data.thickness:
+                self.calc_fit()
+                self.state.last_thickness_fit = self.data.thickness
+            self.calc_reflectance_plot.clear()
+            self.calc_reflectance_plot.plot(self.data.calc_reflectance_spectrum[0], self.data.calc_reflectance_spectrum[1], pen=self.main_pen)
+            if self.data.fit is not None:
+                logger.info(f"Calculated thickness is {self.data.fit.params[0]} +- {self.data.fit.error[0]}")
+                self.calc_reflectance_plot.plot(self.data.fit.wavelength,
+                                                self.data.fit.reflectance, pen=self.fit_pen)
+            self.draw_theoretical_fit()
+        else:
+            self.logger.debug("Can't calculate reflectance")
+
+    def get_thin_film_material(self):
+        logger.debug("Loading thin film material properties")
+        self.state.thin_film_properties_path = QFileDialog.getOpenFileName()[0]
+        if self.state.thin_film_properties_path is not None:
+            self.data.thin_film_optical_properties = analysis.load_optical_properties(self.state.thin_film_properties_path)
+            logger.debug(f"Loaded {self.state.thin_film_properties_path} as thin film")
+            self.draw_calculated_reflectance()
+
+    def get_substrate_material(self):
+        logger.debug("Loading substrate material properties")
+        self.state.substrate_properties_path = QFileDialog.getOpenFileName()[0]
+        if self.state.substrate_properties_path is not None:
+            self.data.substrate_optical_properties = analysis.load_optical_properties(self.state.substrate_properties_path)
+            logger.debug(f"Loaded {self.state.substrate_properties_path} as substrate")
+            self.draw_calculated_reflectance()
+
+    def calc_fit(self):
+        if analysis.can_calculate_thickness(self.data):
+            logger.debug("Calculating thickness")
+            self.data.fit = analysis.calculate_thickness(self.data)
+            self.data.thickness = self.data.fit.params[0]
+            self.set_fit_slider_pos()
+
+    def get_fit_slider_pos(self, val):
+        self.data.thickness = val
+        self.draw_calculated_reflectance()
+        self.thickness_display.setText(f"Thickness: {self.data.thickness} nm")
+
+    def set_fit_slider_pos(self):
+        self.thickness_slider_fit.setValue(self.data.thickness)
+        self.thickness_display.setText(f"Thickness: {self.data.thickness} nm")
+
+    def get_theory_slider_pos(self, val):
+        self.data.thickness_theoretical = val
+        self.thickness_theory_display.setText(f"Theoretical Model Thickness: {self.data.thickness_theoretical} nm")
+        self.draw_calculated_reflectance()
+
+    def get_theory_amplitude_slider_pos(self, val):
+        self.data.amplitude_theoretical = val
+        self.amplitude_theory_display.setText(f"Theoretical Model Amplitude: {self.data.amplitude_theoretical}")
+        self.draw_calculated_reflectance()
+
+    def draw_theoretical_fit(self):
+        theo = lambda x: analysis.reflectance_model(x, 1,
+                                                    self.data.thin_film_optical_properties.n,
+                                                    self.data.thin_film_optical_properties.k,
+                                                    self.data.substrate_optical_properties.n,
+                                                    self.data.substrate_optical_properties.k,
+                                                    self.data.thickness_theoretical, self.data.amplitude_theoretical)
+        self.calc_reflectance_plot.plot(self.data.fit.wavelength, theo(self.data.fit.wavelength), pen=self.fit_pen)
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
